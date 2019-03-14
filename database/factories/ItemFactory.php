c01f357ad24c9788b87fe12eaeed14d82887889d
= <=>
2019-03-14 10:13:18 +0530

    Models, And Migrations Completed

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -4,6 +4,14 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Item::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name,
+        'item_pic' => $faker->url,
+        'is_available' => $faker->randomElement(['yes', 'no']),
+        'price' => $faker->numberBetween(100, 200),
+        'description' => $faker->text,
+        'category_id' => function() {
+            return factory(App\Category::class)->create()->id;
+        },
+        'food_type' => $faker->randomElement(['veg','non veg']),
     ];
 });
