40b2f151b3a6fb2f7b09754a7a3013320f1cd54f
= <=>
2019-04-13 10:23:52 +0530

    Made User ID Visible

--- app/Http/Controllers/API/OrdersController.php
+++ app/Http/Controllers/API/OrdersController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Order;
+use App\OrderItem;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -9,7 +11,27 @@ class OrdersController extends BaseController
 {
     //
     public function create(Request $request) {
-        $orderInformation = json_decode($request->json()->all(), true);
-        return dd($orderInformation['user_id']);
+        $data = $request->json()->all();
+
+        $result = json_decode(json_encode($data),true);
+
+        //Get User Id
+        $userId = $result['user_id'];
+        $order = new Order([
+            'user_id' => $userId,
+            'order_status' => "NOT ACCEPTED",
+            'amount' => $result['amount']
+        ]);
+        $order->save();
+        foreach ($result['product_orders'] as $item) {
+
+            $orderItem = new OrderItem([
+                'item_id' => $item['product_id'],
+                'quantity' => $item['quantity']
+            ]);
+            $order->items()->save($orderItem);
+        }
+        return $this->sendResponse("Success", 'User register successfully.');
+
     }
 }
--- app/User.php
+++ app/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'email_verified_at', 'id'
+        'email_verified_at'
     ];
 
     /**
